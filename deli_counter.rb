e6a8b3b11818db95914539db044b5d72a427934c
Alice <agenkin1@gmail.com>
2020-06-18 09:29:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-  array.each_with_index do |value, index| puts "The line is currently: #{index.to_i+1}. #{value} "
+  array.each_with_index do |value, index| puts "The line is currently: #{index.to_i+1}.#{value} "
   end
 end
 end
