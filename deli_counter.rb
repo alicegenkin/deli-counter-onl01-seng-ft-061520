45f3631cd41207e96ba4db27ae3289d56ac9d6c8
Alice <agenkin1@gmail.com>
2020-06-18 09:26:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-  array.each_with_index do |value, index| puts "#{index.to_i+1}"
+  array.each_with_index do |value, index| puts " #{value} : #{index.to_i+1}"
   end
 end
 end
