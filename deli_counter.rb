e48f1cd266b44350b4d802b2ad59c9f59fc1b128
Alice <agenkin1@gmail.com>
2020-06-18 09:50:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(array,name)
 if array.length == 0
   puts "The line is currently empty."
 else
-  puts "Welcome #{name}, You are number #{array.length+1} in line."
+  puts "Welcome #{name}, You are number #{array.length} in line."
 end
 end
 
