20bd765db77824fd214d30ad308ad17ec5cde3d1
Alice <agenkin1@gmail.com>
2020-06-18 09:20:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,7 @@ katz_deli = []
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-  end
-else do
+else
   array.each_with_index do |value, index| puts "#{index_to_i+1}"
   end
 end
