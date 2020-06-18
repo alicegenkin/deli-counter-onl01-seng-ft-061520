c26ad67bd3e6bd577ef2a8bcc2e97e67c724c195
Alice <agenkin1@gmail.com>
2020-06-18 09:50:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,10 @@ end
 line(katz_deli)
 
 def take_a_number(array,name)
+  array.push(name)
 if array.length == 0
   puts "The line is currently empty."
 else
-  array.push(name)
   puts "Welcome #{name}, You are number #{array.length+1} in line."
 end
 end
